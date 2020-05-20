af28d05410417b87139d590fbdc1eb61ae5ae0b4
Martha30 <53914325+Martha30@users.noreply.github.com>
2020-05-20 09:10:01 -0600

    Create IGrafo.java
    changes new

---
+++ IGrafo.java
@@ -0,0 +1,37 @@
+//Martha Gómez 19501
+//20/05/2020
+public interface IGrafo <V,E> {
+
+    /**
+     * Se agrega un vertice al grafo
+     * @param label Etiqueta para el vertice
+     */
+    void add(V label);
+
+    /**
+     *
+     * Agrega arco entre vertices
+     *
+     * @param vtx1  Etiqueta del vertice 1
+     * @param vtx2  Etiqueta del vertice 2
+     * @param label Arco entre vertice 1 y vertice 2
+     * @return Devuelve verdadero si el Arco fue agregado exitosamente
+     */
+    boolean addEdge(V vtx1, V vtx2, E label);
+
+    /**
+     * Obtener el valor de la ruta mas corta entre vertice 1 y vertice 2
+     * @param label1 vertice1
+     * @param label2 vertice2
+     * @return Devuelve la dstancia mas corta
+     */
+    String getRutaMasCorta(V label1, V label2);
+
+    /**
+     * Encuentra el centro del grafo
+     * @param label
+     * @return
+     */
+    V getCentroGrafo(V label);
+
+}
