a04901f3bea67b65a938a15b2fa630f6aa3b55f6
Martha30 <53914325+Martha30@users.noreply.github.com>
2020-05-20 09:09:23 -0600

    Create Edge.java
    changes

---
+++ Edge.java
@@ -0,0 +1,30 @@
+
+
+public class Edge<V,E> {
+
+    private V vtx1;
+    private V vtx2;
+    private E label;
+    private boolean directed;
+    private boolean visited;
+
+    /**
+     * Constructor
+     * @param vtx1
+     * @param vtx2
+     * @param label
+     * @param directed
+     */
+    public Edge(V vtx1, V vtx2, E label, boolean directed) {
+        this.vtx1 = vtx1;
+        this.vtx2 = vtx2;
+        this.label = label;
+        this.directed = directed;
+        this.visited = false;
+    }
+
+    public V here() {
+        return this.vtx1;
+    }
+
+}
